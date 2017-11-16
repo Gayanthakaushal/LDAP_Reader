5608945b9c8acedeee9014fe7631b64eccf71de7
Gayanthakaushal <gknamarathna@gmail.com>
2017-11-16 19:22:50 +0530

    innicial commit

---
+++ src/main/java/org/wso2/LDAPTest.java
@@ -0,0 +1,118 @@
+package org.wso2;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+/**
+ * LDAP Test
+ */
+public class LDAPTest {
+
+    private static String KEYSTORE = "";
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter LDAP URL");
+        String LDAP_URL = sc.next();
+
+        System.out.println("Enter LDAP USER");
+        String LDAP_USER = sc.next();
+
+        System.out.println("Enter LDAP Password");
+        String LDAP_PASSWORD = sc.next();
+
+        System.out.println("Enter LDAP Search Base");
+        String LDAP_SEARCH_BASE = sc.next();
+
+
+        if(args != null  && args.length == 2) {
+            LDAP_PASSWORD = args[0];
+            KEYSTORE = args[1];
+        }
+
+        System.setProperty("javax.net.ssl.trustStore", KEYSTORE);
+        System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
+
+
+        System.out.println("============  Test is started  ================");
+        System.out.println("== TRUST STORE == : "  + KEYSTORE);
+
+
+        Hashtable<String, String > environment = new Hashtable<String, String >();
+
+        environment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        environment.put(Context.SECURITY_AUTHENTICATION, "simple");
+        environment.put(Context.REFERRAL, "follow");
+        environment.put(Context.PROVIDER_URL, LDAP_URL);
+        environment.put(Context.SECURITY_PRINCIPAL, LDAP_USER);
+        environment.put(Context.SECURITY_CREDENTIALS, LDAP_PASSWORD);
+
+
+        for(int i = 0; i < 100; i++ ) {
+
+            long t1 = System.currentTimeMillis();
+            DirContext ctx = null;
+            NamingEnumeration<SearchResult> results = null;
+            try {
+                ctx = new InitialDirContext(environment);
+                String searchFilter = "(&(objectClass=user)(cn=ennesimoPT00000006.sercommGW))";
+                //String searchFilter = "(&(objectClass=person)(cn=admin))";
+                SearchControls searchControls = new SearchControls();
+                searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+                results = ctx.search(LDAP_SEARCH_BASE, searchFilter, searchControls);
+
+                while (results.hasMore()) {
+                    SearchResult searchResult = null;
+                    try {
+                        searchResult = results.next();
+                        System.out.println("== Name == : " + searchResult.getNameInNamespace());
+                    } catch (Exception e) {
+                        System.out.println("ERROR is occurred with Next");
+                        e.printStackTrace();
+                    }
+                }
+            } catch (NamingException e) {
+                System.out.println("ERROR is occurred with HasMore");
+                e.printStackTrace();
+            } finally {
+                if (results != null) {
+                    try {
+                        results.close();
+                    } catch (NamingException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (ctx != null) {
+                    try {
+                        ctx.close();
+                    } catch (NamingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            long t2 = System.currentTimeMillis();
+
+            System.out.println("============  Time for searching a LDAP Query :  " +  (t2-t1)  + " ms ================");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("=====  Test is finished =====");
+
+        System.exit(0);
+    }
+}
