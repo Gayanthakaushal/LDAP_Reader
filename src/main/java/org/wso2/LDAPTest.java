569f8e78b9c2e00ef8953fc4fe71358b27c12c32
npamudika <rpnpamudika@gmail.com>
2017-11-21 14:12:11 +0530

    Parameterize the tenant user

--- src/main/java/org/wso2/LDAPTest.java
+++ src/main/java/org/wso2/LDAPTest.java
@@ -15,8 +15,6 @@ import java.util.Scanner;
  */
 public class LDAPTest {
 
-    private static String KEYSTORE = "";
-
     public static void main(String[] args){
 
         Scanner sc=new Scanner(System.in);
@@ -33,6 +31,14 @@ public class LDAPTest {
         System.out.println("Enter LDAP Search Base");
         String LDAP_SEARCH_BASE = sc.next();
 
+        System.out.println("Enter Keystore path");
+        String KEYSTORE = sc.next();
+
+        System.out.println("Enter Tenant User which you can successfully login");
+        String SUCCESSFUL_USER = sc.next();
+
+        System.out.println("Enter Tenant User which you get READ Timeout");
+        String READ_TIMEOUT_USER = sc.next();
 
         if(args != null  && args.length == 2) {
             LDAP_PASSWORD = args[0];
@@ -41,11 +47,6 @@ public class LDAPTest {
 
         System.setProperty("javax.net.ssl.trustStore", KEYSTORE);
         System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
-
-
-        System.out.println("============  Test is started  ================");
-        System.out.println("== TRUST STORE == : "  + KEYSTORE);
-
 
         Hashtable<String, String > environment = new Hashtable<String, String >();
 
@@ -56,22 +57,24 @@ public class LDAPTest {
         environment.put(Context.SECURITY_PRINCIPAL, LDAP_USER);
         environment.put(Context.SECURITY_CREDENTIALS, LDAP_PASSWORD);
 
+        DirContext ctx = null;
+        NamingEnumeration<SearchResult> results = null;
+        SearchControls searchControls = new SearchControls();
+        SearchResult searchResult;
+
+        System.out.println("============  Test is started  -  Successful User  ================");
+        System.out.println("== TRUST STORE == : "  + KEYSTORE);
 
         for(int i = 0; i < 100; i++ ) {
-
             long t1 = System.currentTimeMillis();
-            DirContext ctx = null;
-            NamingEnumeration<SearchResult> results = null;
             try {
                 ctx = new InitialDirContext(environment);
-                String searchFilter = "(&(objectClass=user)(cn=ennesimoPT00000006.sercommGW))";
+                String searchFilter = "(&(objectClass="+SUCCESSFUL_USER+")(cn=ennesimoPT00000006.sercommGW))";
                 //String searchFilter = "(&(objectClass=person)(cn=admin))";
-                SearchControls searchControls = new SearchControls();
                 searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
                 results = ctx.search(LDAP_SEARCH_BASE, searchFilter, searchControls);
 
                 while (results.hasMore()) {
-                    SearchResult searchResult = null;
                     try {
                         searchResult = results.next();
                         System.out.println("== Name == : " + searchResult.getNameInNamespace());
@@ -81,7 +84,7 @@ public class LDAPTest {
                     }
                 }
             } catch (NamingException e) {
-                System.out.println("ERROR is occurred with HasMore");
+                System.out.println("ERROR is occurred while searching!");
                 e.printStackTrace();
             } finally {
                 if (results != null) {
@@ -111,7 +114,60 @@ public class LDAPTest {
             }
         }
 
-        System.out.println("=====  Test is finished =====");
+        System.out.println("=====  Test is finished  -  Successful User  =====");
+        System.out.println("============  Test is started  -  READ Timeout User  ================");
+        System.out.println("== TRUST STORE == : "  + KEYSTORE);
+
+        for(int i = 0; i < 100; i++ ) {
+            long t1 = System.currentTimeMillis();
+
+            try {
+                ctx = new InitialDirContext(environment);
+                String searchFilter = "(&(objectClass="+READ_TIMEOUT_USER+")(cn=ennesimoPT00000006.sercommGW))";
+                searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+                results = ctx.search(LDAP_SEARCH_BASE, searchFilter, searchControls);
+
+                while (results.hasMore()) {
+                    try {
+                        searchResult = results.next();
+                        System.out.println("== Name == : " + searchResult.getNameInNamespace());
+                    } catch (Exception e) {
+                        System.out.println("ERROR is occurred with Next");
+                        e.printStackTrace();
+                    }
+                }
+            } catch (NamingException e) {
+                System.out.println("ERROR is occurred while searching!");
+                e.printStackTrace();
+            } finally {
+                if (results != null) {
+                    try {
+                        results.close();
+                    } catch (NamingException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (ctx != null) {
+                    try {
+                        ctx.close();
+                    } catch (NamingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            long t2 = System.currentTimeMillis();
+
+            System.out.println("============  Time for searching a LDAP Query :  " +  (t2-t1)  + " ms ================");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("=====  Test is finished  -  READ Timeout User  =====");
 
         System.exit(0);
     }
